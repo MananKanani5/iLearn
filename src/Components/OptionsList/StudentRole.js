06c11b9c9a216cde3a5b40da3c1d7533c2082beb
Manan Kanani <kanani.manan5@gmail.com>
2022-03-06 13:29:58 +0530

    lazy load images

--- src/Components/OptionsList/StudentRole.js
+++ src/Components/OptionsList/StudentRole.js
@@ -4,13 +4,27 @@ import {NavLink} from "react-router-dom";
 const StudentRole = () => {
   return (
     <>
-      <NavLink className="cursor-pointer hover:text-Primary-color transition ease-in-out duration-400 font-semibold">Assignments</NavLink>
-      <NavLink className="cursor-pointer hover:text-Primary-color transition ease-in-out duration-400 font-semibold">Attendance</NavLink>
-      <NavLink className="cursor-pointer hover:text-Primary-color transition ease-in-out duration-400 font-semibold">Forum</NavLink>
-      <NavLink className="cursor-pointer hover:text-Primary-color transition ease-in-out duration-400 font-semibold">Quiz</NavLink>
-      <NavLink className="cursor-pointer hover:text-Primary-color transition ease-in-out duration-400 font-semibold">Notice</NavLink>
-      <NavLink className="cursor-pointer hover:text-Primary-color transition ease-in-out duration-400 font-semibold">Files</NavLink>
-      <NavLink className="cursor-pointer hover:text-Primary-color transition ease-in-out duration-400 font-semibold">Vote</NavLink>
+      <NavLink  style={({ isActive }) => ({
+          color: isActive ? '#f36730' : '',
+        })} to={""} className="hover:text-Primary-color transition ease-in-out duration-400 font-semibold">Assignments</NavLink>
+      <NavLink  style={({ isActive }) => ({
+          color: isActive ? '#f36730' : '',
+        })} to={""} className="hover:text-Primary-color transition ease-in-out duration-400 font-semibold">Attendance</NavLink>
+      <NavLink  style={({ isActive }) => ({
+          color: isActive ? '#f36730' : '',
+        })} to={""} className="hover:text-Primary-color transition ease-in-out duration-400 font-semibold">Forum</NavLink>
+      <NavLink  style={({ isActive }) => ({
+          color: isActive ? '#f36730' : '',
+        })} to={""} className="hover:text-Primary-color transition ease-in-out duration-400 font-semibold">Quiz</NavLink>
+      <NavLink  style={({ isActive }) => ({
+          color: isActive ? '#f36730' : '',
+        })} to={""} className="hover:text-Primary-color transition ease-in-out duration-400 font-semibold">Notice</NavLink>
+      <NavLink  style={({ isActive }) => ({
+          color: isActive ? '#f36730' : '',
+        })} to={""} className="hover:text-Primary-color transition ease-in-out duration-400 font-semibold">Files</NavLink>
+      <NavLink  style={({ isActive }) => ({
+          color: isActive ? '#f36730' : '',
+        })} to={""} className="hover:text-Primary-color transition ease-in-out duration-400 font-semibold">Vote</NavLink>
     </>
   );
 };
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,21 +1,24 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import logo_black from '../img/logo_black.png';
 import bg from '../img/home/bg.png';
+
 class Home extends React.Component {
 
 
   render() {
     return (
       <>
-        <div className="main py-24 px-10 flex justify-between items-center bg-slate-100  max-sm:flex-wrap max-sm:px-5 max-sm:py-14 fade-in">
+
+      <section className="bg-[#fff7f2] hero py-24 px-10 flex justify-between items-center   max-sm:flex-wrap max-sm:px-5 max-sm:py-14 fade-in">
 
           <div className="img w-2/4 max-sm:w-full max-sm:mb-10 ">
-            <img src={bg} alt="bg" />
+            <img src={bg} alt="bg" loading="lazy" />
           </div>
 
           <div className="text pr-10 max-sm:pr-0 max-sm:flex max-sm:flex-wrap max-sm:justify-center sm:ml-16">
-            <h3 className="tracking-widest uppercase text-[25px] max-sm:text-[18px]">Welcome to kes's</h3>
-            <h1 className=" font-bold uppercase mb-5 text-Secondary-color max-sm:text-[28px] ">Learners College</h1>
+            <h3 className="tracking-widest uppercase max-sm:text-[18px]">Welcome to kes's</h3>
+            <h1 className=" font-bold uppercase mb-5 text-Secondary-color max-sm:text-[28px] max-md:text-[40px]">Learners College</h1>
             <p className="mb-12 max-sm:text-center max-sm:mb-10">Learners College of Arts and Commerce is one of the best colleges in the Mumbai.</p> 
             <button className="">
             <NavLink to={"."}
@@ -30,8 +33,19 @@ class Home extends React.Component {
             </NavLink>
             </button>
           </div>
+        </section>
 
-        </div>
+
+        {/* <section className="bg-white about py-24 px-10 flex justify-between items-center max-sm:flex-wrap max-sm:flex-col-reverse max-sm:px-5 max-sm:py-14 fade-in ">
+          <div className="info flex-1 mr-5 max-sm:flex max-sm:flex-wrap max-sm:justify-center">
+            <h3 className="text-right max-sm:text-[18px]">About Us</h3>
+            <h1 className="font-bold mb-5 text-Secondary-color uppercase text-right max-sm:text-center max-sm:text-[28px]">A little More About Us</h1>
+            <p className="mb-12 text-right max-sm:text-center max-sm:mb-10 sm:pl-24">Learners College of Arts and Commerce is affiliated to University of Mumbai. It is managed by Kandivali Education Society. Various Degree, Diploma and Under Graduate Programmes conducted are of Arts, Commerce, Management and Science. Our promise is to bring the best educational facilities to our students by maintaining a sharp focus on the pursuit of knowledge and skills.</p>
+          </div>
+          <div className="img w-2/4 max-sm:w-full max-sm:mb-10 sm:pl-5">
+            <img src={logo_black} alt="bg" loading="lazy" />
+          </div>
+        </section> */}
       </>
     );
   }
