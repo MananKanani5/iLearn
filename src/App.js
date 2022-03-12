61a95d7c84efdee21044c7fa1b4a5e975d093927
Manan Kanani <kanani.manan5@gmail.com>
2022-03-12 15:06:40 +0530

    Lazy load components & routes

--- src/App.js
+++ src/App.js
@@ -1,39 +1,46 @@
-import React from 'react'
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Navbar from "./Components/Navbar"
-import Footer from './Components/Footer'
-import AdminCreate from './routes/AdminCreate'
-import Attendance from './routes/Attendance'
-import About from './routes/About'
-import Home from './routes/Home'
-import Assignment from './routes/Assignment'
-import Notice from "./routes/Notice";
-import Quiz from "./routes/Quiz";
-import Login from "./routes/Login"
-import StudentCreate from './routes/StudentCreate';
-import TeacherCreate from './routes/TeacherCreate';
-import AssignmentDetails from './routes/AssignmentDetails';
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Navbar from "./Components/Navbar"
+// import Footer from './Components/Footer'
+// import AdminCreate from './routes/AdminCreate'
+// import Attendance from './routes/Attendance'
+// import About from './routes/About'
+// import Home from './routes/Home'
+// import Assignment from './routes/Assignment'
+// import Notice from "./routes/Notice";
+// import Quiz from "./routes/Quiz";
+// import Login from "./routes/Login"
+// import StudentCreate from './routes/StudentCreate';
+// import TeacherCreate from './routes/TeacherCreate';
+// import AssignmentDetails from './routes/AssignmentDetails';
 
-// lazzy loading components import
+// lazy loading components, routes import
 
-// const Navbar = lazy(()=>import('./Components/Navbar'))
-// const Footer = lazy(()=>import('./Components/Footer'))
-// const AdminCreate = lazy(()=>import('./routes/AdminCreate'))
-// const Attendance = lazy(()=>import('./routes/Attendance'))
-// const About = lazy(()=>import('./routes/About'))
-// const Home = lazy(()=>import('./routes/Home'))
-// const Assignment = lazy(()=>import('./routes/Assignment'))
-// const Notice = lazy(()=>import('./routes/Notice'))
-// const Quiz = lazy(()=>import('./routes/Quiz'))
-// const Login = lazy(()=>import('./routes/Login'))
+const Navbar = React.lazy(() => import('./Components/Navbar'));
+const Footer = React.lazy(() => import('./Components/Footer'));
+const AdminCreate = lazy(() => import('./routes/AdminCreate'));
+const Attendance = lazy(()=>import('./routes/Attendance'));
+const About = lazy(() => import('./routes/About'));
+const Home = lazy(() => import('./routes/Home'));
+const Assignment = lazy(() => import('./routes/Assignment'));
+const Notice = lazy(() => import('./routes/Notice'));
+const Quiz = lazy(() => import('./routes/Quiz'));
+const Login = lazy(() => import('./routes/Login'));
+const StudentCreate = lazy(() => import('./routes/StudentCreate'));
+const TeacherCreate = lazy(() => import('./routes/TeacherCreate'));
+const AssignmentDetails = lazy(() => import('./routes/AssignmentDetails'));
 
 function App() {
   return (
     <>
-      <BrowserRouter>   
-      <Navbar />  
-        <Routes>   
+    
+      <Router> 
+          <Suspense fallback={<div>Routes</div>}>
+          <Suspense fallback={<div>Navbar</div>}>
+            <Navbar />
+          </Suspense>
+        <Routes>
           <Route path='/'  element={<Home />} />
           <Route path='/About' element={<About />} />
           <Route path='/Assignment' element={<Assignment />} />
@@ -46,9 +53,11 @@ function App() {
           <Route path='/TeacherCreate' element={<TeacherCreate />} />
           <Route path="/Login" element={<Login />} />
         </Routes>
-      </BrowserRouter>
-    
+        </Suspense>
+      </Router>
+      <Suspense fallback={<div>Footer</div>}>
       <Footer />
+      </Suspense>
     </>
   );
 }
