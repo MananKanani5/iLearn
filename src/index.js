801882b56cb82b1e44fda6b2c0963a5e7e3ac219
Manan Kanani <kanani.manan5@gmail.com>
2022-03-12 12:55:44 +0530

    preloader

--- src/index.js
+++ src/index.js
@@ -3,9 +3,36 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+class Loader extends React.Component {
+  state = {
+    loading: true
+  };
+
+  componentDidMount() {
+    // this simulates an async action, after which the component will render the content
+    demoAsyncCall().then(() => this.setState({ loading: false }));
+  }
+  
+  render() {
+    const { loading } = this.state;
+    
+    if(loading) { // if your component doesn't have to wait for an async action, remove this block 
+      return null; // render null when app is not ready
+    }
+    
+    return (
+      <App />
+    ); 
+  }
+}
+
+function demoAsyncCall() {
+  return new Promise((resolve) => setTimeout(() => resolve(), 1000));
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Loader /> 
   </React.StrictMode>,
   document.getElementById('root')
 );
