8fadc501a7f185fa907031767de3485cf98dd380
Manan <jayrupa210502@gmail.com>
2022-05-02 01:13:24 +0530

    Table

---
+++ src/routes/Dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <>
+         <section className="px-8 max-sm:px-5 py-12 bg-[#fff7f2] m-z fade-in">
+        <div className="bg-white shadow-md rounded-2xl px-8 pt-6 pb-8 mb-4 mx-auto flex">
+
+        </div>
+        </section>
+    </>
+  )
+}
+
+export default Dashboard
