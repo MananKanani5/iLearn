0869dafbdadc67010a05276e2fbd689a771af0a0
Nishant Badlani <nishantbadlani2@gmail.com>
2022-05-01 12:39:40 +0530

    acc chng

--- src/Components/Accordion.js
+++ src/Components/Accordion.js
@@ -2,16 +2,19 @@ import React, { useState } from 'react'
 import data from './data'
 import SingleQuestion from './Question'
 
-const Accordion = () => {
+const Accordion = (props) => {
   const [questions, setQuestions] = useState(data)
-
+  const changeUri = (url) =>
+  {
+    props.changeUrl(url);
+  }
   return (
     <main>
       <div className='bg-white'>
         <h3 className='mb-10 text-2xl font-bold uppercase text-center leading-normal'>Course Content</h3>
         <section className='info'>
           {questions.map((question) => (
-            <SingleQuestion key={question.id} {...question} />
+            <SingleQuestion key={question.id} msg={'hi'} changeUri={changeUri} data={question} />
           ))}
         </section>
       </div>
